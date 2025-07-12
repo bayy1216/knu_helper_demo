d722eaeef75eb7cf263147fa7f45ba6e414ba29d
손홍석 <78216059+bayy1216@users.noreply.github.com>
2025-07-12 22:08:55 +0900

    [Feat]: Uistate 변환

--- core/system/src/main/java/com/reditus/core/system/UiState.kt
+++ core/system/src/main/java/com/reditus/core/system/UiState.kt
@@ -1,7 +1,7 @@
 package com.reditus.core.system
 
-sealed class UiState<out T>{
-    data object Loading : UiState<Nothing>()
-    data class Success<out T>(val data: T) : UiState<T>()
-    data class Error(val exception: Throwable) : UiState<Nothing>()
+sealed class UiState<out T, out E>{
+    data object Loading : UiState<Nothing, Nothing>()
+    data class Success<out T>(val data: T) : UiState<T, Nothing>()
+    data class Error<out E>(val error:E) : UiState<Nothing, E>()
 }
